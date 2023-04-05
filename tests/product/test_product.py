c1be307cb2838c009998abe854e2f3cf87db85a3
danricardo88 <daniel.ricardo88@outlook.com>
2023-04-05 15:26:50 -0300

    req-1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "Cai no golpe",
+        "Não vale 40k",
+        "2022-01-01",
+        "2022-12-31",
+        "12345",
+        "Armazenar em local seco e fresco",
+    )
+
+    assert produto.id == 1
+    assert produto.nome_da_empresa == "Não vale 40k"
+    assert produto.nome_do_produto == "Cai no golpe"
+    assert produto.data_de_fabricacao == "2022-01-01"
+    assert produto.data_de_validade == "2022-12-31"
+    assert produto.numero_de_serie == "12345"
+    assert (
+        produto.instrucoes_de_armazenamento
+        == "Armazenar em local seco e fresco"
+    )
