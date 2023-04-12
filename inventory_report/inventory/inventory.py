78935dd4c29620b85a6edef9cc4727a2cbfe91cf
danricardo88 <daniel.ricardo88@outlook.com>
2023-04-12 09:29:45 -0300

    se for foi... e vou dormir req 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,19 +1,28 @@
 import csv
-from inventory_report.reports.simple_report import SimpleReport
+import json
+
 from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
     @staticmethod
     def import_data(file_path, report_type):
-        if report_type not in ["simples", "completo"]:
-            raise ValueError("Tipo inválido")
-
-        with open(file_path, "r", encoding="utf-8") as file:
-            reader = csv.DictReader(file)
-            data = [dict(row) for row in reader]
-
-        if report_type == "simples":
-            return SimpleReport.generate(data)
-        else:
-            return CompleteReport.generate(data)
+        with open(file_path, "r") as file:
+            if file_path.endswith(".csv"):
+                reader = csv.DictReader(file)
+                data = [dict(row) for row in reader]
+            elif file_path.endswith(".json"):
+                data = json.load(file)
+            else:
+                raise ValueError(
+                    "Tipo de arquivo inválido. Forneça um arquivo CSV ou JSON."
+                )
+            if report_type == "simples":
+                return SimpleReport.generate(data)
+            elif report_type == "completo":
+                return CompleteReport.generate(data)
+            else:
+                raise ValueError(
+                    "Relatório inválido, forneça 'simples' ou 'completo."
+                )
