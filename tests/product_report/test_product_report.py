b52b1ddd554f92790855152887f8dd449cb2427d
danricardo88 <daniel.ricardo88@outlook.com>
2023-04-13 00:41:45 -0300

    req8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
     pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Nascido em",
+        "Utero", "2020-01-01",
+        "humano",
+        "021",
+        "em colo de mamãe"
+    )
+
+    assert str(product) == (
+        "O produto Nascido em"
+        " fabricado em 2020-01-01"
+        " por Utero com validade"
+        " até humano"
+        " precisa ser armazenado em colo de mamãe."
+    )
